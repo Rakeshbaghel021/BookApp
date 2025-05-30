50dd03a2707ca5ac70fcb680de126e37eba9e82f
Rakesh Baghel <rakesh.b@vetron.in>
2025-05-30 12:03:27 +0530

    sec expire

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const generateToken = (userId) => {
   return jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "15s",
   });
 };
 

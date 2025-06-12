652db6f63b911798d98f98af384579712319cdd9
Rakesh Baghel <rakesh.b@vetron.in>
2025-06-12 16:23:50 +0530

    update latest code

--- src/routes/dummyRoutes.js
+++ src/routes/dummyRoutes.js
@@ -1,15 +1,22 @@
 import express from "express";
 import Dummy from "../Models/Dummy.js";
+import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/dummy", async (req, res) => {
+// Get dummy data
+router.get("/", protectRoute, async (req, res) => {
   try {
     const dummyData = await Dummy.find().sort({ createdAt: -1 });
-    res.status(200).json({ data: dummyData });
+
+    if (!dummyData || dummyData.length === 0) {
+      return res.status(404).json({ message: "No New Data found" });
+    }
+
+    res.status(200).json({ success: true, data: dummyData });
   } catch (error) {
-    console.error("Error fetching dummy data:", error);
-    res.status(500).json({ message: "Failed to fetch dummy data" });
+    console.error("Error fetching New data:", error);
+    res.status(500).json({ message: "Server error while fetching New data" });
   }
 });
 
